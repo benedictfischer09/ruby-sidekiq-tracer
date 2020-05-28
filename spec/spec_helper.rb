c56d9c624292653f6377954f734eb4acaf46f5ed
Josh Lauer <jlauer@doximity.com>
2020-05-28 08:59:08 -0400

    lint

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,12 +7,14 @@ require "sidekiq/tracer"
 
 # patch the test tracer to accept and ignore the :ignore_active_scope argument. This is ugly but necessary
 # to avoid forking and updating the test-tracer gem to handle the latest version of OpenTracing
+# rubocop:disable Metrics/ParameterLists, Lint/UnusedMethodArgument, Layout/LineLength
 module TestTracerUpdates
   def start_span(operation_name, child_of: nil, references: nil, start_time: Time.now, tags: nil, ignore_active_scope: false)
     super(operation_name, child_of: child_of, references: references, start_time: start_time, tags: tags)
   end
 end
 Test::Tracer.prepend TestTracerUpdates
+# rubocop:enable Metrics/ParameterLists, Lint/UnusedMethodArgument, Layout/LineLength
 
 Sidekiq::Testing.fake!
 
@@ -31,4 +33,3 @@ RSpec.configure do |config|
     Sidekiq::Worker.clear_all
   end
 end
-
